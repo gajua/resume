685fb3cfb67871434b3c4c63708233a09e4f2e57
sewon kim <sewon0325@naver.com>
2022-12-24 14:31:29 +0900

    모달 on했을 시 버튼 disabled

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const modalOnBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal");
-const closeBtn = modal.querySelector(".close-icon");
+const modalOnBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal');
+const closeBtn = modal.querySelector('.close-icon');
+const body = document.querySelector('body');
+const button = document.querySelectorAll('button')
+
 function onclickBtn() {
-  modal.classList.add("active");
+  modal.classList.add('active');
+  body.classList.add('active')
+  console.log(body.classList);
+  button.forEach((e)=>{
+    e.disabled=true
+  })
+}
+function closeModal() {
+  modal.classList.remove('active');
+  body.classList.remove('active')
+  console.log(body.classList);
+  button.forEach((e)=>{
+    e.disabled=false
+  })
 }
 
-modalOnBtn.addEventListener("click", onclickBtn);
-function closeModal(){
-    modal.classList.remove("active")
-}
-closeBtn.addEventListener('click',closeModal)
+
+
+modalOnBtn.addEventListener('click', onclickBtn);
+closeBtn.addEventListener('click', closeModal);
