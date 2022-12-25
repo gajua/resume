38f0f541b6c6f8377976f2f6dd0132d668e39059
sewon kim <sewon0325@naver.com>
2022-12-25 11:50:57 +0900

    모달생성시 링크 클릭 방지

--- app.js
+++ app.js
@@ -2,26 +2,31 @@ const modalOnBtn = document.querySelector('.modal-btn');
 const modal = document.querySelector('.modal');
 const closeBtn = modal.querySelector('.close-icon');
 const body = document.querySelector('body');
-const button = document.querySelectorAll('button')
+const button = document.querySelectorAll('button');
+const a = document.querySelectorAll('a');
 
 function onclickBtn() {
   modal.classList.add('active');
-  body.classList.add('active')
+  body.classList.add('active');
   console.log(body.classList);
-  button.forEach((e)=>{
-    e.disabled=true
-  })
+  button.forEach((e) => {
+    e.disabled = true;
+  });
+  a.forEach((e) => {
+    e.style.pointerEvents = 'none';
+  });
 }
 function closeModal() {
   modal.classList.remove('active');
-  body.classList.remove('active')
+  body.classList.remove('active');
   console.log(body.classList);
-  button.forEach((e)=>{
-    e.disabled=false
-  })
+  button.forEach((e) => {
+    e.disabled = false;
+  });
+  a.forEach((e) => {
+    e.style.pointerEvents = 'auto';
+  });
 }
-
-
 
 modalOnBtn.addEventListener('click', onclickBtn);
 closeBtn.addEventListener('click', closeModal);
